60d30f6ed247efa41476431f2770514bf693a43c
LucasBeneditoCostaNeves <lucasbene03@gmail.com>
2023-09-11 16:49:59 -0300

    Feat: Base Inicial do Projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { DivTopHeader } from "./components/divTopHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lucas</h1>
+      <DivTopHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/divTopHeader/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const DivTopHeader = () => {
+  return (
+    <div>
+      <h1>Benedito</h1>
+    </div>
+  );
+};
